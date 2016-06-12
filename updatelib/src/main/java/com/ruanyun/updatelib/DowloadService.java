ab9af126d50d6a74a11fdda6a0388a48298a2620
zhangsan <mf1564662361@qq.com>
2016-06-12 23:21:01 +0800

    下载完成安装 具体写法有待考虑

--- updatelib/src/main/java/com/ruanyun/updatelib/DowloadService.java
+++ updatelib/src/main/java/com/ruanyun/updatelib/DowloadService.java
@@ -4,9 +4,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import java.io.File;
 
 import okhttp3.OkHttpClient;
 
@@ -21,6 +24,7 @@ public class DowloadService extends Service implements downLoadListener {
     private NotificationManager manager = null;
     private DownLoadInfo downLoadInfo = null;
 
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,22 +38,18 @@ public class DowloadService extends Service implements downLoadListener {
         super.onCreate();
     }
 
+
     @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
+    public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
             downLoadInfo = intent.getParcelableExtra(DownLoadInfo.DOWNLOAD_INFO);
             dowloader = new OkhttpDowloader(downLoadInfo.downloadUrl,
                     new OkHttpClient().newBuilder().build(), downLoadInfo.downloadPath);
             dowloader.setDownloadListener(this);
-        }
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (dowloader != null) {
             dowloader.startDownload();
         }
+
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -70,6 +70,13 @@ public class DowloadService extends Service implements downLoadListener {
 
     @Override
     public void onDownloadComplete() {
+        Uri uri = Uri.fromFile(new File(new StringBuilder(downLoadInfo.downloadPath)
+                .append("/").append(downLoadInfo.fileName).toString()));
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setDataAndType(uri,
+                "application/vnd.android.package-archive");
+        startActivity(intent);
         stopSelf();
     }
 
